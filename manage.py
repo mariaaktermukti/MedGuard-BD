aca8346889f1145c22590906d1d8933121c37cef
mariaaktermukti <mariamukti051@gmail.com>
2026-08-11 09:45:59 +0600

    Update manage config

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medguard.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
     try:
         # pyrefly: ignore [missing-import]
         from django.core.management import execute_from_command_line
@@ -16,6 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
